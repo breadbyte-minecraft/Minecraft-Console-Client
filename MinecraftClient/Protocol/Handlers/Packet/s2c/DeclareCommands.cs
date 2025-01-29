36bff5d86d9a178021067c9095ca9f89f92de4ac
breadbyte <14045257+breadbyte@users.noreply.github.com>
2025-01-30 06:51:58 +0800

    Add a null check for CollectSignArguments
    i don't know what it does, but it sure doesn't throw an IndexOutOfBoundsException anymore

--- MinecraftClient/Protocol/Handlers/Packet/s2c/DeclareCommands.cs
+++ MinecraftClient/Protocol/Handlers/Packet/s2c/DeclareCommands.cs
@@ -185,6 +185,9 @@ namespace MinecraftClient.Protocol.Handlers.packet.s2c
 
         private static void CollectSignArguments(int NodeIdx, string command, List<Tuple<string, string>> arguments)
         {
+            if (Nodes.Length == 0)
+                return;
+            
             CommandNode node = Nodes[NodeIdx];
             string last_arg = command;
             switch (node.Flags & 0x03)
